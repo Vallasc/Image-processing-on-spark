9afe11f18f27249ff053edbd6c42f947946a16a6
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-04-07 10:42:02 +0200

    Clean code

--- src/main/scala/Image.scala
+++ src/main/scala/Image.scala
@@ -1,109 +1,85 @@
 import java.awt.image.{BufferedImage, DataBufferByte}
 import java.io.File
 import javax.imageio.ImageIO
+import java.awt.image.Raster
+import java.awt.image.WritableRaster
+import java.awt.image.SampleModel
+import java.awt.image.DataBufferInt
+import java.awt.image.ColorModel
 
-class Image {
+class Image(imageFile: File) {
 
-  def getARGBPixelMatrix(inputFile: File) : Array[Array[Int]] = {
-    val image: BufferedImage = ImageIO.read(inputFile)
-    val pixels = image.getRaster.getDataBuffer.asInstanceOf[DataBufferByte].getData
-    val width = image.getWidth
-    val height = image.getHeight
-    val hasAlphaChannel = image.getAlphaRaster != null
+    var image: BufferedImage = null
+    var width: Int = 0
+    var height: Int = 0
+    var hasAlphaChannel = false
+    var pixelMatrix: Array[Int] = null
 
-    val result = Array.ofDim[Int](height, width)
-    val pixelLength = if (hasAlphaChannel) 4 else 3
-    var pixel = 0
-    var row = 0
-    var col = 0
-    println(hasAlphaChannel)
-    while(pixel + pixelLength -1 < pixels.length) {
-      var argb = 0
-      if(hasAlphaChannel) {
-        argb += (pixels(pixel) & 0xff) << 24 // alpha
-        argb += (pixels(pixel + 1) & 0xff) // blue
-        argb += ((pixels(pixel + 2) & 0xff) << 8) // green
-        argb += ((pixels(pixel + 3) & 0xff) << 16) // red
-      } else {
-        argb += -16777216 // 255
-        argb += (pixels(pixel) & 0xff) // blue
-        argb += ((pixels(pixel + 1) & 0xff) << 8) // green
-        argb += ((pixels(pixel + 2) & 0xff) << 16) // red
-      }
-      result(row)(col) = argb
+    def getPixelMatrix(greyScale: Boolean = false) : Array[Int] = {
+        image = ImageIO.read(imageFile)
+        width = image.getWidth
+        height = image.getHeight
+        hasAlphaChannel = image.getAlphaRaster != null
 
-      col += 1
-      if (col == width) {
-        col = 0
-        row += 1
-      }
-      pixel += pixelLength
-    }
-    result
-  }
-
-  def getGreyScalePixelMatrix(inputFile: File) : Array[Array[Int]] = {
-    val matrix = getARGBPixelMatrix(inputFile)
-    var row = 0
-    while (row < matrix.length){
-      var col = 0
-      while(col < matrix(0).length){
-        val pixel = fromARGBtoGreyScale(matrix(row)(col))
-        //matrix(row)(col) = (pixel * (upperBound - lowerBound) / 255) - lowerBound
-        matrix(row)(col) = pixel
-        col += 1
-      }
-      row += 1
-    }
-    matrix
-  }
-
-  def getGreyScalePixelMatrix(pixelMatrix: Array[Array[Int]]) : Array[Array[Int]] = {
-    var row = 0
-    while (row < pixelMatrix.length){
-      var col = 0
-      while(col < pixelMatrix(0).length){
-        val pixel = fromARGBtoGreyScale(pixelMatrix(row)(col))
-        //matrix(row)(col) = (pixel * (upperBound - lowerBound) / 255) - lowerBound
-        pixelMatrix(row)(col) = pixel
-        col += 1
-      }
-      row += 1
-    }
-    pixelMatrix
-  }
-  // 0.3 * R + 0.6 * G + 0.1 * B
-  private def fromARGBtoGreyScale(pixel: Int) : Int = {
-    val r = (pixel >> 16) & 0xff
-    val g = (pixel >> 8) & 0xff
-    val b = (pixel) & 0xff
-    (r*0.3 + g*0.6 + b*0.1).toInt
-  }
-
-  private def fromGreyToRGB(pixel: Int) : Int = {
-    val value = pixel & 0xff
-    val r = value << 16
-    val g = value << 8
-    val b = value
-    r | g | b
-  }
-
-  def saveImage(outputFile: File, pixelMatrix: Array[Array[Int]], isGrey: Boolean) : Unit = {
-    val width = pixelMatrix(0).length
-    val height = pixelMatrix.length
-
-    val bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB );
-    var row = 0
-    while(row < height) {
-      var col = 0
-      while (col < width) {
-        val pixel = if( isGrey) fromGreyToRGB(pixelMatrix(row)(col)) else pixelMatrix(row)(col)
-        bufferedImage.setRGB(col, row, pixel)
-        col += 1
-      }
-      row += 1
+        val outMatrix: Array[Int] = new Array[Int](width * height)
+        pixelMatrix = new Array[Int](width * height)
+        val pixels = image.getRaster.getDataBuffer.asInstanceOf[DataBufferByte].getData
+        val pixelLength = if (hasAlphaChannel) 4 else 3
+        for { i <- 0 until pixels.length/pixelLength } {
+            var argb = 0
+            val pixel = i * pixelLength
+            if(hasAlphaChannel) {
+                argb += (pixels(pixel) & 0xff) << 24 // alpha
+                argb += (pixels(pixel + 1) & 0xff) // blue
+                argb += ((pixels(pixel + 2) & 0xff) << 8) // green
+                argb += ((pixels(pixel + 3) & 0xff) << 16) // red
+            } else {
+                argb += -16777216 // 255
+                argb += (pixels(pixel) & 0xff) // blue
+                argb += ((pixels(pixel + 1) & 0xff) << 8) // green
+                argb += ((pixels(pixel + 2) & 0xff) << 16) // red
+            }
+            pixelMatrix(i) = argb
+            outMatrix(i) = if (greyScale) fromARGBtoGreyScale(argb) else argb
+        }
+        outMatrix
     }
 
-    ImageIO.write(bufferedImage, "PNG", outputFile)
-  }
+    def setPixelMatrix(pixelMatrix: Array[Int], width: Int, height: Int, greyScale: Boolean = false) = {
+        this.width = width
+        this.height = height
+        if (greyScale)
+            this.pixelMatrix = pixelMatrix.map(pixel => fromGreyToARGB(pixel))
+        else
+            this.pixelMatrix = pixelMatrix
+    }
+
+    def saveImage = {
+        val buffer: DataBufferInt = new DataBufferInt(pixelMatrix, pixelMatrix.length)
+
+        val bandMasks: Array[Int] = Array(0xFF0000, 0xFF00, 0xFF, 0xFF000000) // ARGB (yes, ARGB, as the masks are R, G, B, A always) order
+        val raster: WritableRaster = Raster.createPackedRaster(buffer, width, height, width, bandMasks, null)
+
+        val cm: ColorModel = ColorModel.getRGBdefault()
+        val bufferedImage = new BufferedImage(cm, raster, cm.isAlphaPremultiplied(), null)
+        bufferedImage.setData(raster)
+        ImageIO.write(bufferedImage, "PNG", imageFile)
+    }
+
+    // 0.3 * R + 0.6 * G + 0.1 * B
+    private def fromARGBtoGreyScale(pixel: Int) : Int = {
+        val r = (pixel >> 16) & 0xff
+        val g = (pixel >> 8) & 0xff
+        val b = (pixel) & 0xff
+        (r*0.3 + g*0.6 + b*0.1).toInt
+    }
+
+    private def fromGreyToARGB(pixel: Int) : Int = {
+        val value = pixel & 0xff
+        val a = -16777216
+        val r = value << 16
+        val g = value << 8
+        val b = value
+        a | r | g | b
+    }
 }
