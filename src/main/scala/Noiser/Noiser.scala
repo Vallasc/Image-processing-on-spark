ca8de14c266ec0f906e29052e64cccabbe24f6ea
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-07-01 12:39:30 +0200

    Add noiser to produce noisy images

---
+++ src/main/scala/Noiser/Noiser.scala
@@ -0,0 +1,33 @@
+import java.io.File
+import scala.util.Random
+import _root_.Utils.FileUtils
+
+object Noiser {
+    val PROB = 0.3
+    val inputImage = "./data/4.jpg"
+    val outputImage = "./data/test_4.png"
+
+    def main(args: Array[String]) = {
+        println("Start noiser")
+        println("Image: " + inputImage)
+
+        val is = FileUtils.getInputStream(inputImage)
+        val imageIn = new Image()
+        var matrix = imageIn.getPixelMatrix(is, true)
+        is.close()
+
+        val out = matrix.map( pixel => {
+            if ( Random.nextDouble < PROB ) {
+                if(pixel > 128) 0 else 255
+            } else pixel
+        })
+
+        val os = FileUtils.getOutputStream(outputImage)
+        val imageOut = new Image()
+        imageOut.setPixelMatrix(out, imageIn.width, imageIn.height, true)
+        imageOut.saveImage(os)
+        os.close()
+
+        println("End noiser")
+    }
+}
